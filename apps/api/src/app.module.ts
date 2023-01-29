09c811cff81146cdc774cd1e7643f8606bc88c02
Lucas Vallejo <lucasvallejo92@gmail.com>
2023-01-29 11:17:55 -0300

    ✨ feat: add products cart and order modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { CartModule } from './cart/cart.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, CartModule, OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
