e08a4f7d093b3d0a10c80e2317b13598ceec2659
Lucas Vallejo <lucasvallejo92@gmail.com>
2023-02-01 22:05:58 -0300

    ✨ feat(web): add 404 page

--- apps/web/src/pages/NotFound.tsx
+++ apps/web/src/pages/NotFound.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import { Box, Typography } from '@mui/material';
 
-const NotFound = () => {
-
-  return (
-    <>
-      Not Found Page
-    </>
-  )
-}
+const NotFound = () => (
+  <Box>
+    <Typography variant="h3" display="flex" alignItems="center" justifyContent="center" height="calc(100vh - 64px)">
+      404 Not Found
+    </Typography>
+  </Box>
+);
 
 export default NotFound;
