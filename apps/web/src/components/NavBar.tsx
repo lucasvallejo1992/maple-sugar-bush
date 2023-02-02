57d30bc8c88a1ba500b24c928e142fb8da6ae952
Lucas Vallejo <lucasvallejo92@gmail.com>
2023-02-01 22:37:07 -0300

    ♻️ refactor(web): clean nav bar component

--- apps/web/src/components/NavBar.tsx
+++ apps/web/src/components/NavBar.tsx
@@ -1,19 +1,18 @@
 import { AppBar, Toolbar, Typography } from '@mui/material';
-import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useCart } from '../hooks/useCart';
 
+import { useCart } from '../hooks/useCart';
 import CartButton from './common/CartButton';
 
 const NavBar = () => {
   const navigate = useNavigate();
-  const { productList, productsCount } = useCart();
+  const { productsCount } = useCart();
 
   return (
     <AppBar position="static" color="transparent" elevation={4}>
       <Toolbar sx={{ justifyContent: 'space-between' }}>
         <Typography variant="h6" component="div" sx={{ cursor: 'pointer' }} onClick={() => navigate('/')}>
-          Maplr
+          Maple Sugar
         </Typography>
         <CartButton count={productsCount} onClick={() => navigate('/cart')} />
       </Toolbar>
